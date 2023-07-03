21470de2449beb25208ef0ae8886eef27b4b8e0e
Rihanul Islam <rihanthedev@gmail.com>
2023-07-03 17:28:54 +0600

    Catch Routing On Next JS

---
+++ src/app/profile/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfilePage = () => {
+    return (
+        <div>
+            <h3 className="text-2xl">Mrs Hoot Welcome Your Profile</h3>
+        </div>
+    );
+};
+
+export default ProfilePage;
