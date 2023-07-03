e6b445d198bc3ada15d3183fcb1f7515515384d3
Rihanul Islam <rihanthedev@gmail.com>
2023-07-03 16:54:07 +0600

    Relevent path

---
+++ src/components/BigSection/Big.jsx
@@ -0,0 +1,13 @@
+import useMe from '@/Hooks/useMe';
+import React from 'react';
+
+const Big = () => {
+    const heloo = useMe
+    return (
+        <div>
+            hello iam from Big Section
+        </div>
+    );
+};
+
+export default Big;
---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import useMe from '../../Hooks/useMe'
+const HeroSection = () => {
+    const useHim = useMe
+    return (
+        <div>
+            <h2>Hello Iam Form Next js</h2>
+        </div>
+    );
+};
+
+export default HeroSection;
