b683aecb09573c8e56a36542682f5cb16146bdb1
uniquecoderrihan <rihanthedev@gmail.com>
2023-10-22 19:20:07 +0600

    pages

---
+++ src/app/track-activity/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Trackpage = () => {
+    return (
+        <div>
+            <p className="3xl text-center font-thin">Welcome Admin Panel!</p>
+            <p>oreeeeeeeeee</p>
+        </div>
+    );
+};
+
+export default Trackpage;
